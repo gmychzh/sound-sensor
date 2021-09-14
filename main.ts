1263b1a4fd1c0de8c872e9fedd3adaf940bea821
gmychzh <90561143+gmychzh@users.noreply.github.com>
2021-09-14 21:02:20 +0700

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    if (mbit_传感器类.Voice_Sensor(DigitalPin.P0, mbit_传感器类.enVoice.Voice)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.pause(1000)
+    }
+})
